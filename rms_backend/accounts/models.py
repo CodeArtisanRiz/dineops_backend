b62869259864bd28718d631efbd46636e302ad02
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-17 20:46:40 +0530

    Updated User model and db

--- rms_backend/accounts/models.py
+++ rms_backend/accounts/models.py
@@ -9,11 +9,17 @@ class Tenant(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     has_hotel_feature = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
+class User(AbstractUser):
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    is_tenant_admin = models.BooleanField(default=False)
+
+# # Temp fix for superuser -- first time only
 # class User(AbstractUser):
-#     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+#     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, null=True, blank=True)
 #     is_tenant_admin = models.BooleanField(default=False)
 
-# Temp fix for superuser -- first time only
-class User(AbstractUser):
-    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, null=True, blank=True)
-    is_tenant_admin = models.BooleanField(default=False)
+    # def __str__(self):
+    #     return self.name
