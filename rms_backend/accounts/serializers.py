75d0050322857b884ea79602e0923975bd8fa5ed
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-17 21:20:07 +0530

    Added Comments

--- rms_backend/accounts/serializers.py
+++ rms_backend/accounts/serializers.py
@@ -1,15 +1,20 @@
+# Import necessary modules
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Tenant
 
+# Get the custom User model
 UserModel = get_user_model()
 
+# Serializer for User model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = ['id', 'username', 'email', 'tenant', 'is_tenant_admin']
 
+# Serializer for Tenant model
 class TenantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tenant
-        fields = '__all__'
+
+        fields = '__all__'  # Include all fields from the Tenant model
--- rms_backend/accounts/views.py
+++ rms_backend/accounts/views.py
@@ -1,4 +1,5 @@
-# from django.shortcuts import render
+
+# Import necessary modules
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
 from .models import Tenant
@@ -6,18 +7,23 @@ from .serializers import UserSerializer, TenantSerializer
 from rest_framework.permissions import IsAuthenticated
 
 
-# Create your views here.
+
+# Get the User model
 UserModel = get_user_model()
 
+# ViewSet for Tenant model
 class TenantViewSet(viewsets.ModelViewSet):
     queryset = Tenant.objects.all()
     serializer_class = TenantSerializer
 
+# ViewSet for User model
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
+        # Get the current user
         user = self.request.user
+        # Filter queryset to only include users from the same tenant
         return self.queryset.filter(tenant=user.tenant)
