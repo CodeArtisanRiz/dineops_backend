c9689db19dc0b4d65fa1aa67960a7c2fd94429f7
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-17 21:20:07 +0530

    Added Comments

--- rms_backend/accounts/management/commands/createsuperusertenant.py
+++ rms_backend/accounts/management/commands/createsuperusertenant.py
@@ -2,16 +2,20 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 from accounts.models import Tenant
 
+# Get the custom User model
 User = get_user_model()
 
 class Command(BaseCommand):
     help = 'Create a superuser with a tenant'
 
     def handle(self, *args, **kwargs):
+        # Get tenant information from user input
         tenant_name = input('Enter tenant name: ')
         tenant_domain_url = input('Enter tenant domain URL: ')
+        # Create a new Tenant object
         tenant = Tenant.objects.create(name=tenant_name, domain_url=tenant_domain_url)
 
+        # Loop until a unique username is provided
         while True:
             username = input('Enter superuser username: ')
             if User.objects.filter(username=username).exists():
@@ -19,9 +23,11 @@ class Command(BaseCommand):
             else:
                 break
 
+        # Get superuser email and password
         email = input('Enter superuser email: ')
         password = input('Enter superuser password: ')
 
+        # Create a new superuser with the provided information
         user = User.objects.create_superuser(
             username=username,
             email=email,
@@ -29,4 +35,5 @@ class Command(BaseCommand):
             tenant=tenant,
             is_tenant_admin=True
         )
+        # Display success message
         self.stdout.write(self.style.SUCCESS(f'Successfully created superuser {username} with tenant {tenant_name}'))
--- rms_backend/accounts/models.py
+++ rms_backend/accounts/models.py
@@ -4,22 +4,30 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class Tenant(models.Model):
+    # Name of the tenant
     name = models.CharField(max_length=100)
+    # Unique URL for the tenant's domain
     domain_url = models.URLField(unique=True)
+    # Timestamp for when the tenant was created
     created_at = models.DateTimeField(auto_now_add=True)
+    # Flag to indicate if the tenant has access to hotel features
     has_hotel_feature = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
 
 class User(AbstractUser):
+    # Foreign key relationship to the Tenant model
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    # Flag to indicate if the user is an admin for their tenant
     is_tenant_admin = models.BooleanField(default=False)
 
-# # Temp fix for superuser -- first time only
+
+
+# Temp fix for superuser -- first time only
 # class User(AbstractUser):
+#     # Optional foreign key relationship to the Tenant model for superuser
 #     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, null=True, blank=True)
+#     # Flag to indicate if the user is an admin for their tenant
 #     is_tenant_admin = models.BooleanField(default=False)
 
-    # def __str__(self):
-    #     return self.name
