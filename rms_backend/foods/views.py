c1d062d0de6c97fc278f14df96daab31317762ed
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-07-23 00:19:10 +0530

    Added fields in foodItem: created_at, modified_at[], created_by, modified_by:[]

--- rms_backend/foods/views.py
+++ rms_backend/foods/views.py
@@ -12,7 +12,6 @@ class FoodItemViewSet(viewsets.ViewSet):
 
     def get_queryset(self):
         user = self.request.user
-        # Fix: Return all food items if the user is a superuser
         if user.is_superuser:
             return FoodItem.objects.all()
         return FoodItem.objects.filter(tenant=user.tenant)
@@ -29,39 +28,41 @@ class FoodItemViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        user = request.user
+        user = self.request.user
         if user.is_superuser:
             tenant_id = request.data.get('tenant')
             if not tenant_id:
                 raise PermissionDenied("Superuser must include tenant ID in request.")
             serializer = FoodItemSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save(tenant_id=tenant_id)
+                serializer.save(created_by=user, tenant_id=tenant_id)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         elif user.role in ['admin', 'manager']:
             serializer = FoodItemSerializer(data=request.data)
             if serializer.is_valid():
-                serializer.save(tenant=user.tenant)
+                serializer.save(created_by=user, tenant=user.tenant)
                 return Response(serializer.data, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             raise PermissionDenied("You do not have permission to perform this action.")
 
     def update(self, request, pk=None):
-        user = request.user
-        if not user.is_superuser and user.role != 'manager':
+        user = self.request.user
+        if not user.is_superuser and user.role not in ['admin', 'manager']:
             raise PermissionDenied("You do not have permission to perform this action.")
         queryset = self.get_queryset()
         food_item = get_object_or_404(queryset, pk=pk)
         serializer = FoodItemSerializer(food_item, data=request.data, partial=True)
         if serializer.is_valid():
-            serializer.save()
+            modified_by_list = food_item.modified_by
+            modified_by_list.append(user.username)
+            serializer.save(modified_by=modified_by_list)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def destroy(self, request, pk=None):
-        user = request.user
+        user = self.request.user
         if not user.is_superuser and user.role != 'manager':
             raise PermissionDenied("You do not have permission to perform this action.")
         queryset = self.get_queryset()
