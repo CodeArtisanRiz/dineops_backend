87771984b31755f44f122312545207e2ce606da0
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-08-18 15:08:28 +0530

    Modularized:upload_image and handle_image_upload; Added Category: Nested Category in FoodItems > Simplified Response

--- rms_backend/foods/models.py
+++ rms_backend/foods/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 from django.conf import settings
 from accounts.models import Tenant
-from datetime import datetime
 
+class Category(models.Model):
+    STATUS_CHOICES = [
+        ('enabled', 'Enabled'),
+        ('disabled', 'Disabled'),
+    ]
+    tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    image = models.URLField(null=True, blank=True)  # Store URL instead of image file
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='enabled')
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.JSONField(default=list, blank=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, related_name='created_categories')
+    modified_by = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return self.name
 class FoodItem(models.Model):
     STATUS_CHOICES = [
         ('enabled', 'Enabled'),
@@ -14,7 +30,9 @@ class FoodItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     # image = models.ImageField(upload_to='foods/images', null=True, blank=True)
     image = models.URLField(null=True, blank=True)  # Store URL instead of image file
-    category = models.CharField(max_length=100, blank=True, null=True)
+    # category = models.CharField(max_length=100, blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, related_name='food_items')
+    veg = models.BooleanField(default=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='enabled')
     created_at = models.DateTimeField(auto_now_add=True)
     # Temp Fix for existing entries
