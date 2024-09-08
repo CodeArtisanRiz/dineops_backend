c2ede0c7f5841bae723f8f4e59b3b978ba7c5fcf
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-09-08 23:12:57 +0530

    Fix admin ui

--- rms_backend/settings.py
+++ rms_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG flag from .env
-# DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 
