41a474b55fbe4a14186bc94c8950257037468224
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-09-16 23:17:05 +0530

    Handle subscription if subcription_to/from is null

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -45,6 +45,8 @@ class TenantSerializer(serializers.ModelSerializer):
 
     def get_subscription(self, obj):  # New method to determine subscription status
         from datetime import date
+        if obj.subscription_from is None or obj.subscription_to is None:
+            return None
         return obj.subscription_from <= date.today() <= obj.subscription_to
 # class TenantSerializer(serializers.ModelSerializer):
 #     class Meta:
