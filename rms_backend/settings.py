46eb9987d5479a3d0e25c4f09523153c0059b1d9
H M Rizwan Mazumder <riz1.xtreme@gmail.com>
2024-11-14 14:37:06 +0530

    Removed CONN_MAX_AGE

--- rms_backend/settings.py
+++ rms_backend/settings.py
@@ -154,7 +154,7 @@ DATABASES = {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'connect_timeout': 28800,
         },
-        'CONN_MAX_AGE': 600,  # Keep connections open for 10 minutes
+        # 'CONN_MAX_AGE': 600,  # Keep connections open for 10 minutes
     }
 }
 
